11acf35adb85732fa375e23909780203379ce542
Robert Bergeron <robertbergeron3@gmail.com>
2019-12-29 15:54:39 -0800

    Rewrote more header files to use C

--- astree.h
+++ astree.h
@@ -1,68 +1,51 @@
 #ifndef __ASTREE_H__
 #define __ASTREE_H__
 
-#include <iostream>
-#include <string>
-#include <vector>
-#include <unordered_map>
-
-using namespace std;
+#include <stdio.h>
+#include <string.h>
 
 #include "auxlib.h"
 #include "lyutils.h"
-
-struct symbol_value;
-enum class attr: long unsigned int;
+#include "map.h"
+#include "vector.h"
 
 struct location {
-   size_t filenr {};
-   size_t linenr {};
-   size_t offset {};
+   size_t filenr;
+   size_t linenr;
+   size_t offset;
 };
-
-ostream& operator<< (ostream&, const location&);
 
 struct astree {
-   using attr_bitset = bitset<static_cast<long unsigned int>(16)>;
-   using symbol_table = unordered_map<const string*,symbol_value*>;
-   using symbol_entry = symbol_table::value_type;
-
-   // Fields.
-   int symbol;                   // token code
-   location loc;                 // source location
-   location decl_loc;            // for identies declaration location
-   const string* lexinfo;        // pointer to lexical information
-   vector<astree*> children;     // children of this n-way node
-   astree* next_sibling;         // for adopting long lists of siblings
-   astree* firstborn;            // head of the list of siblings
-   size_t block_nr = 0;          // block number this node occurs in
-   attr_bitset attributes;       // type attributes
-   const string* type_id = nullptr; // structure type
-   static const char* NOINFO;    // use indicates no lexical info
-
-   // Functions.
-   astree (int symbol, const location&, const char* lexinfo);
-   ~astree();
-   astree* adopt (astree* child1, astree* child2 = nullptr,
-         astree* child3 = nullptr);
-   astree* adopt_sym (int symbol, astree* child1,
-         astree* child2 = nullptr);
-   astree* buddy_up (astree* sibling);
-   astree* first();
-   astree* second();
-   astree* third();
-   bool has_attr(attr attribute);
-   void set_attr(attr attribute);
-   void dump_node (ostream&);
-   void dump_tree (ostream&, int depth = 0);
-   static void dump (ostream& out, astree* tree);
-   static void print (ostream& out, astree* tree, int depth = 0);
+   int symbol;                     // token code
+   struct location loc;            // source location
+   struct location decl_loc;       // for identies declaration location
+   const char **lexinfo;           // pointer to lexical information
+   struct vector *children;        // children of this n-way node
+   struct astree *next_sibling;    // for adopting long lists of siblings
+   struct astree *firstborn;       // head of the list of siblings
+   size_t block_nr;                // block number this node occurs in
+   int *attributes;                // type attributes
+   const char **type_id;           // structure type
 };
 
-ostream& operator<< (ostream&, const astree*);
+void location_print (FILE *out, const struct location location_);
 
-void destroy (astree* tree1, astree* tree2 = nullptr,
-      astree* tree3 = nullptr);
+struct astree *astree_init (int symbol, const struct location,
+        const char *lexinfo);
+void astree_free (struct astree *astree_);
+struct astree *astree_adopt(struct astree *parent, struct astree *child1,
+        struct astree *child2, struct astree *child3);
+struct astree *astee_adopt_sym (struct astree *parent, int symbol,
+        struct astree *child1, struct astree *child2);
+struct astree *astree_buddy_up(struct astree *astree_, struct astree *sibling);
+void astree_dump_node (struct astree *astree_, FILE *out);
+void astree_dump_tree (struct astree *astree_, FILE *out, int depth);
+void astree_print (const struct astree *astree_, FILE *out);
+
+// it might be a good idea to combine this and astree_free into a single
+// function somehow
+void destroy (struct astree* tree1, struct astree* tree2,
+      struct astree* tree3);
 
 #endif
 
