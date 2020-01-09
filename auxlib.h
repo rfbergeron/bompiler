e4e9c5db2d34d58853a4b059d1c56cf49809d270
rfbergeron <robertbergeron3@gmail.com>
2020-01-08 22:50:52 -0800

    Debug macros are now variadic; should make them easier to use.

--- auxlib.h
+++ auxlib.h
@@ -18,10 +18,11 @@
 void debug_set_flags (const char *optflags);
 int debug_get_flag (char flag);
 void debug_where (char flag, const char *file, int line,
-                  const char *pretty_function, const char *msg);
-void debug_where_short (char flag, const char *file, int line, const char *msg);
+                  const char *pretty_function, const char *msg, ...);
+void debug_where_short (char flag, const char *file, int line,
+                        const char *msg, ...);
 
-// DEBUGF -
+// DEBUGL -
 //    Macro which expands into debug code.  First argument is a
 //    debug flag char, second argument is output code that can
 //    be sandwiched between <<.  Beware of operator precedence.
@@ -30,31 +31,36 @@ void debug_where_short (char flag, const char *file, int line, const char *msg);
 //    will print two words and a newline if flag 'u' is  on.
 //    Traces are preceded by filename, line number, and function.
 //
-// DEBUGH -
-//    DEBUGF, but shortens debug statments by not printing the
+// DEBUGS -
+//    DEBUGL, but shortens debug statments by not printing the
 //    long __PRETTY_FUNCTION__ macro
+//
+// DEBUGE -
+//    Macro which expands into debug code. Prints identical information to
+//    DEBUGS, but without a message. Instead, one or more lines of code, which
+//    are enclosed in the braces, are executed if the appropriate flag is set.
 
 #    ifdef NDEBUG
-#        define DEBUGF(FLAG,CODE) ;
-#        define DEBUGS(FLAG,STMT) ;
-#        define DEBUGH(FLAG,CODE) ;
+#        define DEBUGL(FLAG,PRINTF_ARGS...) ;
+#        define DEBUGE(FLAG,PRINTF_ARGS...) ;
+#        define DEBUGS(FLAG,PRINTF_ARGS...) ;
 #    else
-#        define DEBUGF(FLAG,CODE) { \
+#        define DEBUGL(FLAG,PRINTF_ARGS...) { \
              if (debug_get_flag (FLAG)) { \
                  debug_where (FLAG, __FILE__, __LINE__, \
-                        __PRETTY_FUNCTION__, CODE); \
+                        __PRETTY_FUNCTION__, PRINTF_ARGS); \
              } \
          }
-#        define DEBUGS(FLAG,STMT) { \
+#        define DEBUGE(FLAG,STMT) { \
              if (debug_get_flag (FLAG)) { \
                  debug_where (FLAG, __FILE__, __LINE__, \
                         __PRETTY_FUNCTION__, ""); \
                  STMT; \
              } \
          }
-#        define DEBUGH(FLAG,CODE) { \
+#        define DEBUGS(FLAG,PRINTF_ARGS...) { \
              if (debug_get_flag (FLAG)) { \
-                 debug_where_short (FLAG, __FILE__, __LINE__, CODE); \
+                 debug_where_short (FLAG, __FILE__, __LINE__, PRINTF_ARGS); \
              } \
          }
 #    endif
