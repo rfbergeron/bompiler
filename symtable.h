96282ede88131fa5e79179d595d360b773c1c18c
rfbergeron <robertbergeron3@gmail.com>
2020-02-06 20:33:53 -0800

    Lots of progress on symbol table; debug macro flushes output correctly now

--- symtable.h
+++ symtable.h
@@ -9,7 +9,8 @@
 #include "lyutils.h"
 #include "klib/khash.h"
 
-KHASH_MAP_INIT_INT(symbol_table, struct symbol_value *);
+typedef struct SymbolValue SymbolValue;
+KHASH_MAP_INIT_STR(symbol_table, SymbolValue *);
 
 // attributes correspond to array indices in the order they are listed here
 enum attr {
@@ -28,25 +29,47 @@ enum attr {
     ATTR_LVAL,
     ATTR_CONST,
     ATTR_VREG,
-    ATTR_VADDR
+    ATTR_VADDR,
+    NUM_ATTRIBUTES
 };
 
-struct symbol_value {
-    int attributes[16];
+char attr_map[][32] = {
+    "void",
+    "int",
+    "null",
+    "string",
+    "struct",
+    "array",
+    "function",
+    "variable",
+    "field",
+    "typeid",
+    "parameter",
+    "local",
+    "lval",
+    "const",
+    "vreg",
+    "vaddr"
+};
+
+struct SymbolValue {
+    int attributes[(size_t) NUM_ATTRIBUTES];
     size_t sequence;
     khash_t(symbol_table) *fields;
     Location loc;
     size_t block_nr;
-    struct vector *parameters;
-    const char **type_id;
+    kvec_t(SymbolValue *) parameters;
+    const char *type_id;
     int has_block;
 };
 
-struct symbol_value *symbol_value_init (ASTree *tree,
+SymbolValue *symbol_value_init (ASTree *tree,
                                         size_t sequence_,
                                         size_t block_nr_);
-void symbol_value_free (struct symbol_value *symbol_value_);
-void symbol_value_print (struct symbol_value *symbol_value_, FILE *out);
+void symbol_value_free (SymbolValue *symbol_value_);
+void symbol_value_print (SymbolValue *symbol_value_, FILE *out);
+void type_checker_init_globals ();
+void type_checker_free_globals ();
 
 // may need functions for printing attributes and attribute sets
 
