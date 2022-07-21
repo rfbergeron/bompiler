f0c8f563ef3446d8cfeee091a2d5b1cd78463379
rfbergeron <robertbergeron3@gmail.com>
2022-07-21 11:36:20 -0700

    Check For Lvalues on Assignments
    Check for the lvalue attribute when validating assignments. Do not mark
    typedefs as lvalues.

--- src/bcc_err.c
+++ src/bcc_err.c
@@ -218,6 +218,8 @@ int erraux_to_string(AuxSpec *erraux, char *buf, size_t size) {
     case BCC_TERR_EXPECTED_RETVAL:
       /* TODO(Robert): not usable as implemented */
       return sprintf(buf, "Semantic error: expected return value expression");
+    case BCC_TERR_EXPECTED_LVAL:
+      return expected_err_to_string(erraux, "lvalue", buf, size);
     case BCC_TERR_UNEXPECTED_LIST: {
       ASTree *dest = erraux->data.err.info[0];
       char dest_buf[LINESIZE];
