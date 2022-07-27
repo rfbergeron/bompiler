ee86f039910f0e3c99476ddce4d7808d7e0de871
Robert <rbergero@ucsc.edu>
2022-07-27 12:32:37 -0700

    Implement New Jump Checking Functions
    Implement jump checking stack functions in state.c. Modify simplestack.h
    to be useful for this purpose, since it was unused and the information
    required by break and continue statements is too simple for badlib's
    linked list to be a good solution. More planning on ways to simplify
    compiler or fix broken/unimplemented functionality.

--- src/simplestack.h
+++ src/simplestack.h
@@ -3,50 +3,50 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define DECLARE_STACK(stack_name, stack_type)                                 \
-  struct stack_name {                                                         \
-    stack_type *stack;                                                        \
-    size_t size;                                                              \
-    size_t max_size;                                                          \
-  };                                                                          \
-  void stack_name##_init(struct stack_name *stack, size_t max_size) {         \
-    stack->max_size = max_size;                                               \
-    stack->size = 0;                                                          \
-    stack->stack = malloc(max_size * sizeof(stack_type));                     \
+#define DECLARE_STACK(stack_name, stack_type) \
+  struct stack_name {                         \
+    stack_type *stack;                        \
+    size_t count;                             \
+    size_t size;                              \
+  };
+#define PROC_STACK(stack_name, stack_type)                                    \
+  void stack_name##_init(struct stack_name *stack, size_t size) {             \
+    stack->size = size;                                                       \
+    stack->count = 0;                                                         \
+    stack->stack = malloc(size * sizeof(stack_type));                         \
   }                                                                           \
   void stack_name##_destroy(struct stack_name *stack) { free(stack->stack); } \
   void stack_name##_push(struct stack_name *stack, stack_type value) {        \
-    if (stack->size >= stack->max_size) {                                     \
-      size_t new_max_size = stack->max_size * 2;                              \
-      stack_type *new_stack = malloc(new_max_size * sizeof(stack_type));      \
-      memcpy(new_stack, stack->stack, stack->size * sizeof(stack_type));      \
+    if (stack->count >= stack->size) {                                        \
+      size_t new_size = stack->size * 2;                                      \
+      stack_type *new_stack = malloc(new_size * sizeof(stack_type));          \
+      memcpy(new_stack, stack->stack, stack->count * sizeof(stack_type));     \
       free(stack->stack);                                                     \
-      stack->max_size = new_max_size;                                         \
+      stack->size = new_size;                                                 \
       stack->stack = new_stack;                                               \
     }                                                                         \
-    stack->stack[stack->size] = value;                                        \
-    ++stack->size;                                                            \
+    stack->stack[stack->count++] = value;                                     \
   }                                                                           \
-  void stack_name##_pop(struct stack_name *stack) {                           \
-    --stack->size;                                                            \
-    if (stack->size <= stack->max_size / 4) {                                 \
-      size_t new_max_size = stack->max_size / 2;                              \
-      stack_type *new_stack = malloc(new_max_size * sizeof(stack_type));      \
-      memcpy(new_stack, stack->stack, new_max_size * sizeof(stack_type));     \
+  stack_type stack_name##_pop(struct stack_name *stack) {                     \
+    if (stack->count <= stack->size / 4) {                                    \
+      size_t new_size = stack->size / 2;                                      \
+      stack_type *new_stack = malloc(new_size * sizeof(stack_type));          \
+      memcpy(new_stack, stack->stack, new_size * sizeof(stack_type));         \
       free(stack->stack);                                                     \
-      stack->max_size = new_max_size;                                         \
+      stack->size = new_size;                                                 \
       stack->stack = new_stack;                                               \
     }                                                                         \
+    return stack->stack[--stack->count];                                      \
   }                                                                           \
   stack_type stack_name##_top(struct stack_name *stack) {                     \
-    return stack->stack[stack->size];                                         \
+    return stack->stack[stack->count - 1];                                    \
   }                                                                           \
   void stack_name##_replace(struct stack_name *stack, stack_type value) {     \
-    stack->stack[stack->size] = value;                                        \
+    stack->stack[stack->count - 1] = value;                                   \
   }                                                                           \
   stack_type stack_name##_postfix_inc(struct stack_name *stack) {             \
-    return (stack->stack[stack->size])++;                                     \
+    return (stack->stack[stack->count - 1])++;                                \
   }                                                                           \
-  size_t stack_name##_size(struct stack_name *stack) { return stack->size; }
+  size_t stack_name##_count(struct stack_name *stack) { return stack->count; }
 
 #endif
