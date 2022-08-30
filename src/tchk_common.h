2e699997adbfe3236af540a170305d78db65c93f
rfbergeron <robertbergeron3@gmail.com>
2022-08-30 11:29:41 -0700

    Add Assignment Validity Check
    Add function for checking the validity of types used in assignment
    operators and between function parameters and call arguments. Move
    `is_const_zero` into `tchk_common` and have it be more thorough in
    checking whether or not an expression evaluates to zero.

--- src/tchk_common.h
+++ src/tchk_common.h
@@ -11,6 +11,8 @@ int aux_equivalent(const AuxSpec *aux1, const AuxSpec *aux2,
                    unsigned int flags);
 int types_equivalent(const TypeSpec *type1, const TypeSpec *type2,
                      unsigned int flags);
+int is_const_zero(ASTree *tree);
+int types_assignable(const TypeSpec *dest_type, ASTree *src);
 void arithmetic_conversions(ASTree *operator, const TypeSpec * type1,
                             const TypeSpec *type2);
 void pointer_conversions(ASTree *expr);
