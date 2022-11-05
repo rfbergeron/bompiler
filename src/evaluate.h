001a823d85e7a789e6ed07f0c9a98aced9bc9ec0
Robert <rbergero@ucsc.edu>
2022-11-05 13:00:12 -0700

    Constexpr Evaluation for Struct Member References
    Attempt constexpr evaluation for the arrow and reference operators.
    Combine `translate_reference` and `translate_arrow` into a single
    function.

--- src/evaluate.h
+++ src/evaluate.h
@@ -10,6 +10,7 @@ ASTree *evaluate_conditional(ASTree *qmark, ASTree *condition,
                              ASTree *true_expr, ASTree *false_expr);
 ASTree *evaluate_subscript(ASTree *subscript, ASTree *pointer, ASTree *index);
 ASTree *evaluate_addrof(ASTree *addrof, ASTree *operand);
+ASTree *evaluate_reference(ASTree *reference, ASTree *struct_, ASTree *member);
 ASTree *evaluate_binop(ASTree *operator, ASTree * left, ASTree *right);
 ASTree *evaluate_unop(ASTree *operator, ASTree * operand);
 #endif
