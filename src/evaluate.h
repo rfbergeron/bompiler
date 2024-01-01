23c7aa0d4282381693c8cd186ce215ffdd73a984
Robert <rbergero@ucsc.edu>
2024-01-01 14:13:01 -0800

    Constant Expression Evaluation Fixes
    - fix loading of constant expressions when constant expression evaluation
    fails
    - remove liveness and symbol tracking information from PIC mode operands
    - fix `UNOP_CASE` macro checking operator syntax tree node's flags instead
    of the operand syntax tree node's flags (again)
    - add separate evaluation function for automatic conversions
    - fix `define_enumerator` not unwrapping the `enum` keyword's syntax
    tree node in the case that an error occurred

--- src/evaluate.h
+++ src/evaluate.h
@@ -7,6 +7,7 @@ ASTree *evaluate_charcon(ASTree *charcon);
 ASTree *evaluate_stringcon(ASTree *stringcon);
 ASTree *evaluate_ident(ASTree *ident);
 ASTree *evaluate_cast(ASTree *cast, ASTree *expr);
+ASTree *evaluate_auto_conv(ASTree *auto_conv, ASTree *expr);
 ASTree *evaluate_conditional(ASTree *qmark, ASTree *condition,
                              ASTree *true_expr, ASTree *false_expr);
 ASTree *evaluate_subscript(ASTree *subscript, ASTree *pointer, ASTree *index);
