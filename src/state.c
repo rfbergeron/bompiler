fb69af9e7e8584712cc57432f56e11f7eb75f488
Robert <rbergero@ucsc.edu>
2024-04-22 16:14:17 -0700

    Replace Badlib Linked List in `state`
    Replace badlib's linked list implementation with a managed array, which
    is much simpler and accomplishes the same task just as well.

--- src/state.c
+++ src/state.c
@@ -21,10 +21,12 @@ typedef struct switch_info {
 } SwitchInfo;
 
 struct compiler_state {
-  LinkedList scopes;
+  Scope **scopes;
+  size_t scopes_len, scopes_cap;
+  SwitchInfo **switches;
+  size_t switches_len, switches_cap;
   SizetStack break_stack;
   SizetStack continue_stack;
-  LinkedList switch_stack;
   Symbol *enclosing_function;
   const char *enclosing_function_name;
   size_t jump_id_count;
@@ -32,8 +34,12 @@ struct compiler_state {
 
 CompilerState *state_init(void) {
   CompilerState *state = malloc(sizeof(*state));
-  llist_init(&state->scopes, NULL, NULL);
-  llist_init(&state->switch_stack, NULL, NULL);
+  state->scopes_len = 0;
+  state->scopes_cap = 4;
+  state->scopes = malloc(state->scopes_cap * sizeof(*state->scopes));
+  state->switches_len = 0;
+  state->switches_cap = 1;
+  state->switches = malloc(state->switches_cap * sizeof(*state->switches));
   size_t_stack_init(&state->break_stack, 4);
   size_t_stack_init(&state->continue_stack, 4);
   state->enclosing_function = NULL;
@@ -44,118 +50,130 @@ CompilerState *state_init(void) {
 
 void state_destroy(CompilerState *state) {
   if (state == NULL) return;
-  int status = llist_destroy(&state->scopes);
-  if (status) abort();
-  status = llist_destroy(&state->switch_stack);
-  if (status) abort();
+  free(state->scopes);
+  free(state->switches);
   size_t_stack_destroy(&state->break_stack);
   size_t_stack_destroy(&state->continue_stack);
   free(state);
 }
 
 void state_enter_file(CompilerState *state, const ASTree *root) {
-  assert(llist_empty(&state->scopes));
+  assert(state->scopes_len == 0);
   assert(root->tok_kind == TOK_ROOT);
   assert(root->scope != NULL);
   assert(scope_get_kind(root->scope) == SCOPE_FILE);
-  int status = llist_push_front(&state->scopes, root->scope);
-  if (status) abort();
+
+  state->scopes[state->scopes_len++] = root->scope;
 }
 
 void state_leave_file(CompilerState *state) {
-  assert(llist_size(&state->scopes) == 1);
+  assert(state->scopes_len == 1);
   assert(scope_get_kind(state_peek_scope(state)) == SCOPE_FILE);
-  (void)llist_pop_front(&state->scopes);
+
+  --state->scopes_len;
 }
 
 void state_enter_function(CompilerState *state, const ASTree *declarator,
                           const ASTree *body) {
-  assert(llist_size(&state->scopes) == 1);
+  assert(state->scopes_len == 1);
   assert(scope_get_kind(state_peek_scope(state)) == SCOPE_FILE);
   assert(declarator->tok_kind == TOK_IDENT);
   assert(body->scope != NULL);
   assert(scope_get_kind(body->scope) == SCOPE_FUNCTION);
   assert(state->enclosing_function == NULL);
   assert(state->enclosing_function_name == NULL);
+
   (void)state_get_symbol(state, declarator->lexinfo,
                          &state->enclosing_function);
+
   assert(state->enclosing_function != NULL);
   assert(type_is_function(state->enclosing_function->type));
   state->enclosing_function_name = declarator->lexinfo;
-  int status = llist_push_front(&state->scopes, body->scope);
-  if (status) abort();
+  state->scopes[state->scopes_len++] = body->scope;
 }
 
 void state_leave_function(CompilerState *state) {
-  assert(!llist_empty(&state->scopes));
+  assert(state->scopes_len == 2);
   assert(scope_get_kind(state_peek_scope(state)) == SCOPE_FUNCTION);
   assert(state->enclosing_function != NULL);
   assert(state->enclosing_function_name != NULL);
+
   state->enclosing_function = NULL;
   state->enclosing_function_name = NULL;
-  (void)llist_pop_front(&state->scopes);
+  --state->scopes_len;
 }
 
 void state_enter_prototype(CompilerState *state, const ASTree *param_list) {
-  assert(!llist_empty(&state->scopes));
+  assert(state->scopes_len != 0);
   assert(param_list->scope != NULL);
   assert(state_peek_scope(state) != param_list->scope);
   assert(param_list->tok_kind == TOK_PARAM_LIST);
   assert(scope_get_kind(param_list->scope) == SCOPE_FUNCTION);
-  int status = llist_push_front(&state->scopes, param_list->scope);
-  if (status) abort();
+
+  if (state->scopes_len == state->scopes_cap)
+    state->scopes = realloc(state->scopes,
+                            (state->scopes_cap *= 2) * sizeof(*state->scopes));
+  state->scopes[state->scopes_len++] = param_list->scope;
 }
 
 void state_leave_prototype(CompilerState *state) {
-  assert(!llist_empty(&state->scopes));
+  assert(state->scopes_len > 1);
   assert(scope_get_kind(state_peek_scope(state)) == SCOPE_FUNCTION);
-  (void)llist_pop_front(&state->scopes);
+
+  --state->scopes_len;
 }
 
 void state_enter_block(CompilerState *state, const ASTree *block) {
-  assert(!llist_empty(&state->scopes));
+  assert(state->scopes_len >= 2);
   assert(block->scope != NULL);
   assert(scope_get_kind(block->scope) == SCOPE_BLOCK);
   assert(state_peek_scope(state) != block->scope);
-  int status = llist_push_front(&state->scopes, block->scope);
-  if (status) abort();
+
+  if (state->scopes_len == state->scopes_cap)
+    state->scopes = realloc(state->scopes,
+                            (state->scopes_cap *= 2) * sizeof(*state->scopes));
+  state->scopes[state->scopes_len++] = block->scope;
 }
 
 void state_leave_block(CompilerState *state) {
-  assert(!llist_empty(&state->scopes));
+  assert(state->scopes_len > 2);
   assert(scope_get_kind(state_peek_scope(state)) == SCOPE_BLOCK);
-  (void)llist_pop_front(&state->scopes);
+
+  --state->scopes_len;
 }
 
 void state_enter_record(CompilerState *state, const Tag *tag) {
-  assert(!llist_empty(&state->scopes));
+  assert(state->scopes_len != 0);
   assert(tag->record.kind != TAG_ENUM);
   assert(tag->record.members != NULL);
   assert(scope_get_kind(tag->record.members) == SCOPE_MEMBER);
   assert(state_peek_scope(state) != tag->record.members);
-  int status = llist_push_front(&state->scopes, tag->record.members);
-  if (status) abort();
+
+  if (state->scopes_len == state->scopes_cap)
+    state->scopes = realloc(state->scopes,
+                            (state->scopes_cap *= 2) * sizeof(*state->scopes));
+  state->scopes[state->scopes_len++] = tag->record.members;
 }
 
 void state_leave_record(CompilerState *state) {
-  assert(!llist_empty(&state->scopes));
+  assert(state->scopes_len > 1);
   assert(scope_get_kind(state_peek_scope(state)) == SCOPE_MEMBER);
-  (void)llist_pop_front(&state->scopes);
+
+  --state->scopes_len;
 }
 
 Scope *state_peek_scope(CompilerState *state) {
-  return llist_front(&state->scopes);
+  return state->scopes[state->scopes_len - 1];
 }
 
 int state_get_symbol(CompilerState *state, const char *ident, Symbol **out) {
   size_t i, skipped;
-  for (i = 0, skipped = 0; i < llist_size(&state->scopes); ++i) {
-    Scope *current = llist_get(&state->scopes, i);
+  for (i = 1, skipped = 1; i <= state->scopes_len; ++i) {
+    Scope *current = state->scopes[state->scopes_len - i];
     if (scope_get_kind(current) == SCOPE_MEMBER) {
       ++skipped;
       continue;
     }
-    /* TODO(Robert): rewrite symbol table functions */
     *out = scope_get_symbol(current, ident);
     if (*out != NULL) break;
   }
@@ -166,9 +184,9 @@ int state_get_symbol(CompilerState *state, const char *ident, Symbol **out) {
 
 void state_insert_symbol(CompilerState *state, const char *ident,
                          Symbol *symbol) {
-  size_t i = 0;
+  size_t i = 1;
   Scope *current;
-  do current = llist_get(&state->scopes, i++);
+  do current = state->scopes[state->scopes_len - (i++)];
   while (scope_get_kind(current) == SCOPE_MEMBER);
   scope_insert_symbol(current, ident, symbol);
 }
@@ -182,11 +200,11 @@ void state_insert_symbol(CompilerState *state, const char *ident,
  */
 int state_inheritance_valid(CompilerState *state, const char *ident,
                             Symbol *symbol) {
-  assert(llist_size(&state->scopes) > 1);
+  assert(state->scopes_len > 1);
   Symbol *benefactor;
   size_t i;
-  for (i = 0; i < llist_size(&state->scopes); ++i) {
-    Scope *current = llist_get(&state->scopes, i);
+  for (i = 1; i <= state->scopes_len; ++i) {
+    Scope *current = state->scopes[state->scopes_len - i];
     if (scope_get_kind(current) == SCOPE_MEMBER) continue;
     /* TODO(Robert): rewrite symbol table functions */
     benefactor = scope_get_symbol(current, ident);
@@ -211,7 +229,7 @@ int state_inheritance_valid(CompilerState *state, const char *ident,
     benefactor->linkage = LINK_EXT;
     benefactor->storage = STORE_EXT;
     benefactor->info = SYM_HIDDEN;
-    scope_insert_symbol(llist_back(&state->scopes), ident, benefactor);
+    scope_insert_symbol(state->scopes[0], ident, benefactor);
 
     symbol->linkage = LINK_EXT;
     symbol->storage = STORE_EXT;
@@ -225,8 +243,8 @@ int state_inheritance_valid(CompilerState *state, const char *ident,
 
 int state_get_member(CompilerState *state, const char *ident, Symbol **out) {
   size_t i;
-  for (i = 0; i < llist_size(&state->scopes); ++i) {
-    Scope *current = llist_get(&state->scopes, i);
+  for (i = 1; i <= state->scopes_len; ++i) {
+    Scope *current = state->scopes[state->scopes_len - i];
     if (scope_get_kind(current) != SCOPE_MEMBER) {
       *out = NULL;
       return 0;
@@ -235,19 +253,18 @@ int state_get_member(CompilerState *state, const char *ident, Symbol **out) {
     if (*out != NULL) break;
   }
 
-  return i == 0;
+  return i == 1;
 }
 
 void state_insert_member(CompilerState *state, const char *ident,
                          Symbol *symbol) {
-  Scope *top_scope = llist_front(&state->scopes);
-  scope_insert_member(top_scope, ident, symbol);
+  scope_insert_member(state->scopes[state->scopes_len - 1], ident, symbol);
 }
 
 int state_get_tag(CompilerState *state, const char *ident, Tag **out) {
   size_t i, skipped;
-  for (i = 0, skipped = 0; i < llist_size(&state->scopes); ++i) {
-    Scope *current = llist_get(&state->scopes, i);
+  for (i = 1, skipped = 1; i <= state->scopes_len; ++i) {
+    Scope *current = state->scopes[state->scopes_len - i];
     if (scope_get_kind(current) == SCOPE_MEMBER) {
       ++skipped;
       continue;
@@ -261,57 +278,48 @@ int state_get_tag(CompilerState *state, const char *ident, Tag **out) {
 }
 
 void state_insert_tag(CompilerState *state, const char *ident, Tag *tag) {
-  size_t i = 0;
+  size_t i = 1;
   Scope *current;
-  do current = llist_get(&state->scopes, i++);
+  do current = state->scopes[state->scopes_len - (i++)];
   while (scope_get_kind(current) == SCOPE_MEMBER);
   scope_insert_tag(current, ident, tag);
 }
 
 Label *state_get_label(CompilerState *state, const char *ident) {
-  Scope *function_scope =
-      llist_get(&state->scopes, llist_size(&state->scopes) - 2);
-  return scope_get_label(function_scope, ident);
+  return scope_get_label(state->scopes[1], ident);
 }
 
 void state_insert_label(CompilerState *state, const char *ident, Label *label) {
-  Scope *function_table =
-      llist_get(&state->scopes, llist_size(&state->scopes) - 2);
-  scope_insert_label(function_table, ident, label);
+  scope_insert_label(state->scopes[1], ident, label);
 }
 
 size_t state_get_selection_id(CompilerState *state) {
-  if (llist_empty(&state->switch_stack)) {
-    return (size_t)-1L;
-  }
-
-  return ((SwitchInfo *)llist_front(&state->switch_stack))->id;
+  return state->switches_len == 0
+             ? SIZE_MAX
+             : state->switches[state->switches_len - 1]->id;
 }
 
 size_t state_get_case_id(CompilerState *state) {
-  if (llist_empty(&state->switch_stack)) {
-    return (size_t)-1L;
-  }
-
-  return ((SwitchInfo *)llist_front(&state->switch_stack))->case_id;
+  return state->switches_len == 0
+             ? SIZE_MAX
+             : state->switches[state->switches_len - 1]->case_id;
 }
 
 size_t state_get_control_reg(CompilerState *state) {
-  if (llist_empty(&state->switch_stack)) {
-    return (size_t)-1L;
-  }
-
-  return ((SwitchInfo *)llist_front(&state->switch_stack))->control_reg;
+  return state->switches_len == 0
+             ? SIZE_MAX
+             : state->switches[state->switches_len - 1]->control_reg;
 }
 
 int state_get_selection_default(CompilerState *state) {
-  if (llist_empty(&state->switch_stack)) abort();
-  return ((SwitchInfo *)llist_front(&state->switch_stack))->has_default;
+  assert(state->switches_len != 0);
+  return state->switches[state->switches_len - 1]->has_default;
 }
 
 const Type *state_get_control_type(CompilerState *state) {
-  if (llist_empty(&state->switch_stack)) return NULL;
-  return ((SwitchInfo *)llist_front(&state->switch_stack))->control_type;
+  return state->switches_len == 0
+             ? NULL
+             : state->switches[state->switches_len - 1]->control_type;
 }
 
 size_t state_get_break_id(CompilerState *state) {
@@ -335,11 +343,8 @@ size_t state_next_jump_id(CompilerState *state) {
 }
 
 void state_pop_selection_id(CompilerState *state) {
-  if (llist_size(&state->switch_stack) == 0) {
-    abort();
-  }
-
-  free(llist_pop_front(&state->switch_stack));
+  assert(state->switches_len != 0);
+  free(state->switches[--state->switches_len]);
 }
 
 void state_pop_break_id(CompilerState *state) {
@@ -365,25 +370,29 @@ void state_push_selection_id(CompilerState *state, size_t id) {
   info->has_default = 0;
   info->control_reg = next_vreg();
   info->control_type = NULL;
-  llist_push_front(&state->switch_stack, info);
+
+  if (state->switches_len == state->switches_cap)
+    state->switches = realloc(
+        state->switches, (state->switches_cap *= 2) * sizeof(*state->switches));
+  state->switches[state->switches_len++] = info;
 }
 
 void state_inc_case_id(CompilerState *state) {
-  SwitchInfo *info = llist_front(&state->switch_stack);
-  if (info != NULL) ++info->case_id;
+  if (state->switches_len != 0)
+    ++(state->switches[state->switches_len - 1])->case_id;
 }
 
 void state_set_selection_default(CompilerState *state) {
-  SwitchInfo *info = llist_front(&state->switch_stack);
   /* do not call if there is no switch info */
-  assert(info != NULL && info->has_default == 0);
-  info->has_default = 1;
+  assert(state->switches_len != 0);
+  assert(state->switches[state->switches_len - 1]->has_default == 0);
+  state->switches[state->switches_len - 1]->has_default = 1;
 }
 
 void state_set_control_type(CompilerState *state, const Type *type) {
-  SwitchInfo *info = llist_front(&state->switch_stack);
-  assert(info != NULL && info->control_type == NULL);
-  info->control_type = type;
+  assert(state->switches_len != 0);
+  assert(state->switches[state->switches_len - 1]->control_type == NULL);
+  state->switches[state->switches_len - 1]->control_type = type;
 }
 
 void state_push_break_id(CompilerState *state, size_t id) {
